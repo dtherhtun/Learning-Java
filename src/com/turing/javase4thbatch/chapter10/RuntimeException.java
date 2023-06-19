df2307b736a07561b8c50ab46b045f2185733919
D Ther <dtherhtun.cw@gmail.com>
2023-06-19 14:33:22 +0700

    finally rethrow

--- src/com/turing/javase4thbatch/chapter10/RuntimeException.java
+++ src/com/turing/javase4thbatch/chapter10/RuntimeException.java
@@ -15,6 +15,23 @@ public class RuntimeException {
         // return a/b;
     }
 
+    /*
+    static double div(double a, double b){
+        try{
+            if(Double.isNaN(a) || Double.isNaN(b)){
+                throw new java.lang.RuntimeException("One of the operand is NaN");
+            }
+        }catch (Exception e){
+            return 200;
+
+        }finally {
+            // in this case, no return and error from finally executed.
+            throw new java.lang.RuntimeException("Finally rethrow exception");
+        }
+        // return a/b;
+    }
+     */
+
     public static void main(String[] args) {
 
         double result = div(1, Double.NaN);
