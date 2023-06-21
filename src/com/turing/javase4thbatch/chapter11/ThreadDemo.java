e38ea535b3ee0f38e9c3eead4649da05608cdd43
D Ther <dtherhtun.cw@gmail.com>
2023-06-21 13:41:51 +0700

    Thread

---
+++ src/com/turing/javase4thbatch/chapter11/ThreadDemo.java
@@ -0,0 +1,25 @@
+package com.turing.javase4thbatch.chapter11;
+
+class CountThread extends Thread {
+    String name;
+    CountThread(String name){
+        this.name = name;
+    }
+    public void run(){
+        for (int i = 0; i < 10; i++){
+            System.out.println("Thread "+name+" "+i);
+        }
+    }
+}
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        CountThread th1 = new CountThread("one");
+        CountThread th2 = new CountThread("two");
+        CountThread th3 = new CountThread("three");
+
+        th1.start();
+        th2.start();
+        th3.start();
+    }
+}
