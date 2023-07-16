b9479f5eedec3dfd08b5e64a473d343e57e5a5a0
D Ther <dtherhtun.cw@gmail.com>
2023-07-16 20:00:41 +0700

    AssertDemo

---
+++ src/com/turing/javase4thbatch/chapter13/AssertDemo.java
@@ -0,0 +1,10 @@
+package com.turing.javase4thbatch.chapter13;
+
+// use with java -ea AssertDemo
+public class AssertDemo {
+    public static void main(String[] args) {
+        int k = 11;
+        assert k % 2 == 0;
+        System.out.println("K is positive");
+    }
+}
