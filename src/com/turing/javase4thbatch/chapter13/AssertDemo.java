aa18c55ac491d44f0a8a215fcf08b0ad2a77e3ca
D Ther <dtherhtun.cw@gmail.com>
2023-07-17 19:38:20 +0700

    Static import

--- src/com/turing/javase4thbatch/chapter13/AssertDemo.java
+++ src/com/turing/javase4thbatch/chapter13/AssertDemo.java
@@ -1,10 +1,14 @@
 package com.turing.javase4thbatch.chapter13;
 
+import static java.lang.Math.min;
+
 // use with java -ea AssertDemo
 public class AssertDemo {
     public static void main(String[] args) {
         int k = 11;
         assert k % 2 == 0;
         System.out.println("K is positive");
+
+        System.out.println("Math.min "+ min(2, 5));
     }
 }
