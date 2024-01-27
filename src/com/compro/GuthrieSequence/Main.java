4b9ceff71128808eac23ceef2bea8f524139b779
D Ther Htun <dtherhtun.cw@gmail.com>
2024-01-27 21:48:32 +0700

    feat: GuthrieSequence

--- src/com/compro/GuthrieSequence/Main.java
+++ src/com/compro/GuthrieSequence/Main.java
@@ -24,8 +24,33 @@ public class Main {
         }
         return 1;
     }
+    public static int isGuthrieSequenceGod(int[] a) {
+        for (int i=0 ; i < a.length - 1; i++) {
+            int current = a[i];
+            int next = a[i+1];
+            if(current % 2 == 0) {
+                //event
+                if(next != current/2) {
+                    return 0;
+                }
+            }
+            else {
+                //odd
+                if(next != (current*3) + 1) {
+                    return 0;
+                }
+            }
+        }
+
+        //last number must 1
+        if(a[a.length -1] != 1) {
+            return 0;
+        }
+        return 1;
+    }
 
     public static void main(String[] args) {
         System.out.println(isGuthrieSequence(new int[]{7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1}));
+        System.out.println(isGuthrieSequenceGod(new int[]{7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1}));
     }
 }
