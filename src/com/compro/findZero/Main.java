656dc72ba6116c9e5a2a44ec307f92440e5c4e26
D Ther Htun <dtherhtun.cw@gmail.com>
2024-03-04 21:21:54 +0700

    feat: find zero

---
+++ src/com/compro/findZero/Main.java
@@ -0,0 +1,17 @@
+package com.compro.findZero;
+
+public class Main {
+    public static int arrayHasZero(int[] a) {
+        for (int j : a) {
+            if (j == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(arrayHasZero(new int[]{3, 4, 5, 6, 6}));
+        System.out.println(arrayHasZero(new int[]{3, 4, 0, 6, 6}));
+    }
+}
