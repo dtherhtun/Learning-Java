56176ca4daf31d935d0544119a4787ebf636366b
D Ther Htun <dtherhtun.cw@gmail.com>
2024-03-13 23:14:41 +0700

    feat: DoublyLinkList

--- src/com/dsaleetcode/DoublyLinkList/DLL/DoublyLinkList.java
+++ src/com/dsaleetcode/DoublyLinkList/DLL/DoublyLinkList.java
@@ -21,11 +21,20 @@ public class DoublyLinkList {
     }
 
     public void getHead() {
-        System.out.println("head : "+ head.value);
+        if (head == null) {
+            System.out.println("head is null");
+        } else {
+            System.out.println("head : "+ head.value);
+        }
+
     }
 
     public void getTail() {
-        System.out.println("tail : "+ tail.value);
+        if (tail == null) {
+            System.out.println("tail is null");
+        } else {
+            System.out.println("tail : "+ tail.value);
+        }
     }
 
     public void printList() {
@@ -34,5 +43,84 @@ public class DoublyLinkList {
             System.out.println(temp.value);
             temp = temp.next;
         }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public void length(){
+        System.out.println("Length : "+ length);
+    }
+
+//    public Node removeLast() {
+//        Node temp = tail;
+//        if ( length == 0) {
+//            return null;
+//        } else {
+//            tail = temp.prev;
+//            temp.prev = null;
+//            if (tail != null) {
+//                tail.next = null;
+//            }
+//            length--;
+//        }
+//
+//        if (length == 0) {
+//            tail = null;
+//            head = null;
+//        }
+//        return temp;
+//    }
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = tail;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else  {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length --;
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst() {
+        if (length == 0 ) return null;
+        Node temp = head;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
     }
 }
--- src/com/dsaleetcode/DoublyLinkList/DLL/Main.java
+++ src/com/dsaleetcode/DoublyLinkList/DLL/Main.java
@@ -2,10 +2,20 @@ package com.dsaleetcode.DoublyLinkList.DLL;
 
 public class Main {
     public static void main(String[] args) {
-        DoublyLinkList DLL = new DoublyLinkList(7);
+        DoublyLinkList DLL = new DoublyLinkList(1);
+
+        DLL.append(2);
+        DLL.append(3);
+        DLL.append(4);
+        DLL.removeLast();
+
+        DLL.prepend(0);
 
         DLL.getHead();
         DLL.getTail();
         DLL.printList();
+        DLL.length();
+        System.out.println("Remove First "+ DLL.removeFirst().value);
+        DLL.length();
     }
 }
