952d21af6d7c59639b62d41cf3da51dc14e40ac0
D Ther Htun <dtherhtun.cw@gmail.com>
2024-03-30 21:44:01 +0700

    feat: NUnique

---
+++ src/com/compro/sample/NUnique/Main.java
@@ -0,0 +1,27 @@
+package com.compro.sample.NUnique;
+
+public class Main {
+    public static int isNUnique(int[] a, int n) {
+        if (a.length < 2) return 0;
+        int count = 0;
+        for (int i = 0; i < a.length ; i++) {
+            for (int j = i; j < a.length; j++) {
+                if (i != j) {
+                    if (a[i] + a[j] == n) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count == 1 ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isNUnique(new int[]{7, 3, 3, 2, 4}, 6));
+        System.out.println(isNUnique(new int[]{7, 3, 3, 2, 4}, 10));
+        System.out.println(isNUnique(new int[]{7, 3, 3, 2, 4}, 11));
+        System.out.println(isNUnique(new int[]{7, 3, 3, 2, 4}, 8));
+        System.out.println(isNUnique(new int[]{7, 3, 3, 2, 4}, 4));
+        System.out.println(isNUnique(new int[]{1}, 4));
+    }
+}
