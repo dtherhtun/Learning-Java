a1b6057e2b5dbab47c84cbbf86eba1bc5e2f28e1
D Ther Htun <dtherhtun.cw@gmail.com>
2024-04-11 22:40:48 +0700

    feat: isRailroadTie

--- src/com/compro/isRailroadTie/Main.java
+++ src/com/compro/isRailroadTie/Main.java
@@ -16,11 +16,17 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(isRailroadTie(new int[]{1, 2, 0, 3, -18, 0, 2, 2}));
-        System.out.println(isRailroadTie(new int[]{1, 2, 3, 0, 2, 2}));
-        System.out.println(isRailroadTie(new int[]{0, 1, 2, 0, 3, 4}));
-        System.out.println(isRailroadTie(new int[]{1, 2, 0, 0, 3, 4}));
-        System.out.println(isRailroadTie(new int[]{1, -3, 0, 3, -18, 0, 2, 1, 0, 5, 1}));
-        System.out.println(isRailroadTie(new int[]{1, -3, 0, 3, -18}));
+//        System.out.println(isRailroadTie(new int[]{1, 2, 0, 3, -18, 0, 2, 2}));
+//        System.out.println(isRailroadTie(new int[]{1, 2, 3, 0, 2, 2}));
+//        System.out.println(isRailroadTie(new int[]{0, 1, 2, 0, 3, 4}));
+//        System.out.println(isRailroadTie(new int[]{1, 2, 0, 0, 3, 4}));
+//        System.out.println(isRailroadTie(new int[]{1, -3, 0, 3, -18, 0, 2, 1, 0, 5, 1}));
+//        System.out.println(isRailroadTie(new int[]{1, -3, 0, 3, -18}));
+        System.out.println(isRailroadTie(new int[]{1, 2}));
+        System.out.println(isRailroadTie(new int[]{1, 2, 0, 1, 2, 0, 1, 2}));
+        System.out.println(isRailroadTie(new int[]{3, -3, 0, 3, 3, 0, 3, 3, 0, 3, 3}));
+        System.out.println(isRailroadTie(new int[]{0, 0, 0, 0}));
+        System.out.println(isRailroadTie(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+        System.out.println(isRailroadTie(new int[]{1, 3, 0, 3, 5, 0}));
     }
 }
