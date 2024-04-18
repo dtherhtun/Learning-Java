f32a726e36e1e89da4e13a9dc6fc307e5804aa98
D Ther Htun <dtherhtun.cw@gmail.com>
2024-04-19 01:22:34 +0700

    feat: 121

--- src/com/compro/sample/is121Array/Main.java
+++ src/com/compro/sample/is121Array/Main.java
@@ -2,12 +2,11 @@ package com.compro.sample.is121Array;
 
 public class Main {
     public static int is121Array(int[] a) {
-        if (a.length < 3) return 0;
+        if (a.length < 3 || a[0] != 1 || a[a.length - 1] != 1 || a[0] == a[a.length / 2] || a[a.length / 2] != 2)
+            return 0;
 
-        int mid = a.length / 2;
-
-        for (int i = 0; i <= mid; i++) {
-            if (a[i] != a[a.length - 1 - i] || a[0] == a[mid] || a[0] != 1 || a[a.length - 1] != 1 || a[mid] != 2) {
+        for (int i = 0; i <= a.length / 2; i++) {
+            if (a[i] != a[a.length - 1 - i]) {
                 return 0;
             }
         }
