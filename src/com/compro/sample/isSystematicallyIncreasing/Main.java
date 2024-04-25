4fcd25198bdaecf8d7c70d270b77f721451d1d59
D Ther Htun <dtherhtun.cw@gmail.com>
2024-04-25 22:49:46 +0700

    feat: isSystematicallyIncreasing

--- src/com/compro/sample/isSystematicallyIncreasing/Main.java
+++ src/com/compro/sample/isSystematicallyIncreasing/Main.java
@@ -34,8 +34,24 @@ public class Main {
         return 1;
     }
 
+    static int isSystematicallyIncreasingKnight(int[] a) {
+        int index = 0;
+        int i = 0;
+        do {
+            for (int j = 1; j < i + 1; j++) {
+                if (a[index] != j) {
+                    return 0;
+                }
+                index++;
+            }
+            i++;
+        } while (index < a.length);
+
+        return 1;
+    }
+
     public static void main(String[] args) {
-        System.out.println(isSystematicallyIncreasing(new int[]{1, 1, 2, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6}));
+        System.out.println(isSystematicallyIncreasingKnight(new int[]{1, 1, 2, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6}));
         System.out.println(isSystematicallyIncreasing(new int[]{1}));
         System.out.println(isSystematicallyIncreasing(new int[]{1, 2, 1, 2, 3}));
         System.out.println(isSystematicallyIncreasing(new int[]{1, 1, 3}));
