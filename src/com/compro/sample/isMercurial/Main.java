89ca87b3a9f39a0f63080d5f016c76a79c399786
D Ther Htun <dtherhtun.cw@gmail.com>
2024-05-02 00:12:55 +0700

    feat: isMercurial

---
+++ src/com/compro/sample/isMercurial/Main.java
@@ -0,0 +1,41 @@
+package com.compro.sample.isMercurial;
+
+public class Main {
+    static int isMercurial(int[] a) {
+        boolean precedingOneFlag = false;
+        int indexOf1 = -1;
+        int indexOf3 = -1;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 1) {
+                indexOf1 = i;
+            }
+
+            if (a[i] == 3) {
+                indexOf3 = i;
+            }
+
+            if (indexOf1 != -1 && indexOf3 > indexOf1) {
+                precedingOneFlag = true;
+            }
+
+            if (precedingOneFlag && a[i] == 1) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isMercurial(new int[]{1, 2, 10, 3, 15, 1, 2, 2}));
+        System.out.println(isMercurial(new int[]{5, 2, 10, 3, 15, 1, 2, 2}));
+        System.out.println(isMercurial(new int[]{1, 2, 10, 3, 15, 16, 2, 2}));
+        System.out.println(isMercurial(new int[]{3, 2, 18, 1, 0, 3, -11, 1, 3}));
+        System.out.println(isMercurial(new int[]{2, 3, 1, 1, 18}));
+        System.out.println(isMercurial(new int[]{8, 2, 1, 1, 18, 3, 5}));
+        System.out.println(isMercurial(new int[]{3, 3, 3, 3, 3, 3}));
+        System.out.println(isMercurial(new int[]{1}));
+        System.out.println(isMercurial(new int[]{}));
+    }
+}
