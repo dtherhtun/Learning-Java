263784312cfbfbbb5f0634c1f44b83811710328b
D Ther Htun <dtherhtun.cw@gmail.com>
2024-05-01 22:53:12 +0700

    feat: isOneBalanced touch one

---
+++ src/com/compro/sample/isOneBalanced/Main.java
@@ -0,0 +1,37 @@
+package com.compro.sample.isOneBalanced;
+
+public class Main {
+    static int isOneBalanced(int[] a) {
+        if (a.length == 0) return 1;
+        int count = 0;
+        boolean oneFound = false;
+        boolean nonFound = false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 1) {
+                count++;
+                if (nonFound) {
+                    oneFound = true;
+                }
+            } else {
+                if (oneFound) {
+                    return 0;
+                }
+                nonFound = true;
+                count--;
+            }
+        }
+
+        return count == 0 ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isOneBalanced(new int[]{1, 1, 1, 2, 3, -18, 45, 1}));
+        System.out.println(isOneBalanced(new int[]{1, 1, 1, 2, 3, -18, 45, 1, 0}));
+        System.out.println(isOneBalanced(new int[]{1, 1, 2, 3, 1, -18, 26, 1}));
+        System.out.println(isOneBalanced(new int[]{}));
+        System.out.println(isOneBalanced(new int[]{3, 4, 1, 1}));
+        System.out.println(isOneBalanced(new int[]{1, 1, 3, 4}));
+        System.out.println(isOneBalanced(new int[]{3, 3, 3, 3, 3, 3}));
+        System.out.println(isOneBalanced(new int[]{1, 1, 1, 1, 1, 1}));
+    }
+}
