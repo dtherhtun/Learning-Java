b9f600ac90a3e30c0e7cdc2534923d8a1e1a7e02
D Ther Htun <dtherhtun.cw@gmail.com>
2024-06-03 23:19:47 +0700

    feat: isContinuousFactored

--- src/com/compro/sample/isContinuousFactored/Main.java
+++ src/com/compro/sample/isContinuousFactored/Main.java
@@ -16,6 +16,17 @@ public class Main {
         return 0;
     }
 
+    static int isContinuousFactoredNext(int n) {
+        int i = 2;
+        while (i * i <= n) {
+            if (n % i == 0 && n % (i + 1) == 0) {
+                return 1;
+            }
+            i++;
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         System.out.println(isContinuousFactored(6));
         System.out.println(isContinuousFactored(60));
